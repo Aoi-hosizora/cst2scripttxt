fa5b3b88944c4a2b3b223d9983ffe9d93e93c545
AoiHosizora <a970335605@hotmail.com>
2019-02-02 02:58:19 +0800

    Finish First vector return

--- cst2script/main.cpp
+++ cst2script/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <Windows.h>
+#include <vector>
 
 // #include <WinNls.h>
 // #include <stdio.h>
@@ -18,57 +20,134 @@ using namespace std;
 
 char cs2magic[]="CatScene";
 
+wchar_t* c2w(const char *str,int cp)
+{
+	if (cp==-1)
+	{
+		return (wchar_t*)*str;
+	}
+	wchar_t* buffer;
+	if (str)
+	{
+		size_t nu = strlen(str);
+		size_t n = (size_t)MultiByteToWideChar(cp, 0, (const char *)str, int(nu), NULL, 0);
+		buffer = 0;
+		buffer = new wchar_t[n+1];
+		memset(buffer, 0, sizeof(wchar_t)*(n + 1));
+		MultiByteToWideChar(cp, 0, (const char *)str, int(nu), buffer, int(n));
+	}
+	return buffer;
+}
+
+char* wideCharToMultiByte(wchar_t* pWCStrKey)
+{
+	int pSize = WideCharToMultiByte(CP_OEMCP, 0, pWCStrKey, wcslen(pWCStrKey), NULL, 0, NULL, NULL);
+	char* pCStrKey = new char[pSize+1];
+
+	WideCharToMultiByte(CP_OEMCP, 0, pWCStrKey, wcslen(pWCStrKey), pCStrKey, pSize, NULL, NULL);
+	pCStrKey[pSize] = '\0';
+	return pCStrKey;
+}
+
+vector<string> *CSTOpen(string path,string cp) {
+	
+	int codepage = CP_ACP;
+	if (cp=="Shift-JIS")
+		codepage = 932;
+	else if (cp == "GB18030-0")
+		codepage = 936;
+	else if (cp == "UTF-8")
+		codepage = CP_UTF8;
+	else if (cp=="UTF-16")
+		codepage = -1;
+
+	vector<string> *result;
+	fstream cstfile(path,ios::in|ios::binary);
+	if (!cstfile)  // File path error!
+		throw "path error";
+
+	cstfile.seekg(0, cstfile.end);
+	size_t filesize = cstfile.tellg();
+	cstfile.seekg(0, 0);
+
+	auto buffer=new char[filesize];
+	cstfile.read(buffer,filesize);
+	cstfile.close();
+
+	CS2Head *head;
+	head=(CS2Head*)buffer;
+	if (memcmp(head->Magic,cs2magic,8)!=0) // File content error!
+		throw "content error";
+	if(filesize!=0x10+head->SZcompress) // File broken!
+		throw "file broken";
+
+	//////////////////////////////////////////////////////////////////////////
+
+	auto uncompressbuffer=new char[head->SZuncompress+100];
+	memset(uncompressbuffer, 0, head->SZuncompress + 100);
+
+	uLongf uncompsize= head->SZuncompress + 100;
+	int zstatus = uncompress((unsigned char*)uncompressbuffer, &uncompsize, (const Bytef*)buffer + 0x10, head->SZcompress);
+
+	if (Z_OK!= zstatus || uncompsize!=head->SZuncompress)
+		throw "Zlib tennkai error";
+
+	CS2InfoHead* info = (CS2InfoHead*)uncompressbuffer;
+	if (uncompsize- sizeof(CS2InfoHead) !=info->totalsize)
+		throw "file broken";
+
+	char *opcodeptr;
+	opcodeptr = uncompressbuffer + sizeof(CS2InfoHead) + info->opcodeoffset;
+	int entrys = (info->opcodeoffset - info->index1Size) / 4;
+	int *index = (int*)(uncompressbuffer + sizeof(CS2InfoHead) + info->index1Size);
+
+	for (int i=0;i<entrys;i++) {
+		int offset=index[i];
+		if (opcodeptr[offset]==0x1 && opcodeptr[offset+1]==0x20) {
+			if (opcodeptr[offset+2]!=0x0) {
+				wchar_t* tmpwc = c2w((const char*)&opcodeptr[offset + 2],codepage);
+				string TmpString = wideCharToMultiByte(tmpwc);
+				delete tmpwc;
+				tmpwc = nullptr;
+				result->push_back(TmpString);
+			}
+
+		}
+		if (opcodeptr[offset] == 0x1 && opcodeptr[offset + 1] == 0x21) {
+			if (opcodeptr[offset+2]!=0x0)
+			{
+				wchar_t* tmpwc = c2w((const char*)&opcodeptr[offset + 2],codepage);
+				string TmpString = wideCharToMultiByte(tmpwc);
+				delete tmpwc;
+				tmpwc = nullptr;
+				result->push_back(TmpString);
+			}
+		}
+
+		if (opcodeptr[offset] == 0x1 && opcodeptr[offset + 1] == 0x30)
+		{
+			if (opcodeptr[offset+2]==0x30||opcodeptr[offset+2]==0x31||opcodeptr[offset+2]==0x32||opcodeptr[offset+2]==0x33||opcodeptr[offset+2]==0x34||opcodeptr[offset+2]==0x35||opcodeptr[offset+2]==0x36||opcodeptr[offset+2]==0x37||opcodeptr[offset+2]==0x38||opcodeptr[offset+2]==0x39)
+			{
+				wchar_t* tmpwc = c2w((const char*)&opcodeptr[offset + 2],codepage);
+				string TmpString = wideCharToMultiByte(tmpwc);
+				delete tmpwc;
+				tmpwc = nullptr;
+				result->push_back(TmpString);
+			}
+		}
+	}
+	delete buffer;
+	delete uncompressbuffer;
+	return result;
+}
 int main(int argc, char **argv) {
 	try {
 		if (argc!=2) // Permit to input only two parameter: path & codepage(Shift-JIS, GB18030-0, UTF-8)!
-			 throw "arg kazu error";
+			throw "arg kazu error";
 		string path = argv[0], cp = argv[1];
 
-		int codepage = CP_ACP;
-		if (cp=="Shift-JIS")
-			codepage = 932;
-		else if (cp == "GB18030-0")
-			codepage = 936;
-		else if (cp == "UTF-8")
-			codepage = CP_UTF8;
-		else if (cp=="UTF-16")
-			codepage = -1;
-
-		fstream cstfile(path,ios::in|ios::binary);
-		if (!cstfile)  // File path error!
-			throw "path error";
-
-		cstfile.seekg(0, cstfile.end);
-		size_t filesize = cstfile.tellg();
-		cstfile.seekg(0, 0);
-
-		auto buffer=new char[filesize];
-		cstfile.read(buffer,filesize);
-		cstfile.close();
-
-		CS2Head *head;
-		head=(CS2Head*)buffer;
-		if (memcmp(head->Magic,cs2magic,8)!=0) // File content error!
-			throw "content error";
-		if(filesize!=0x10+head->SZcompress) // File broken!
-			throw "file broken";
-
-		//////////////////////////////////////////////////////////////////////////
-
-		auto uncompressbuffer=new char[head->SZuncompress+100];
-		memset(uncompressbuffer, 0, head->SZuncompress + 100);
-
-		uLongf uncompsize= head->SZuncompress + 100;
-		int zstatus = uncompress((unsigned char*)uncompressbuffer, &uncompsize, (const Bytef*)buffer + 0x10, head->SZcompress);
-
-		if (Z_OK!= zstatus || uncompsize!=head->SZuncompress)
-			throw "Zlib tennkai error";
-
-		CS2InfoHead* info = (CS2InfoHead*)uncompressbuffer;
-		if (uncompsize- sizeof(CS2InfoHead) !=info->totalsize)
-			throw "file broken";
-
-
+		vector<string> *res;
+		res = CSTOpen(path,cp);
 	}
 	catch (string e) {
 		
--- cst2script/typedef.h
+++ cst2script/typedef.h
@@ -1,17 +1,17 @@
 #ifndef DEF_H
 #define DEF_H
 
-//
+// 
 //  Code Page Default Values.
-//
-#define CP_ACP                    0           // default to ANSI code page
-#define CP_OEMCP                  1           // default to OEM  code page
-#define CP_MACCP                  2           // default to MAC  code page
-#define CP_THREAD_ACP             3           // current thread's ANSI code page
-#define CP_SYMBOL                 42          // SYMBOL translations
-
-#define CP_UTF7                   65000       // UTF-7 translation
-#define CP_UTF8                   65001       // UTF-8 translation
+// 
+// #define CP_ACP                    0           // default to ANSI code page
+// #define CP_OEMCP                  1           // default to OEM  code page
+// #define CP_MACCP                  2           // default to MAC  code page
+// #define CP_THREAD_ACP             3           // current thread's ANSI code page
+// #define CP_SYMBOL                 42          // SYMBOL translations
+// 
+// #define CP_UTF7                   65000       // UTF-7 translation
+// #define CP_UTF8                   65001       // UTF-8 translation
 
 //
 //  CS2 Engine Struct.
